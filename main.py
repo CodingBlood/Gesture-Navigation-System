8d930f728e674b0f4e550a49d5460c9d66c69a44
Kartik Agarwal <kartik.agarwal.192.168@gmail.com>
2021-09-10 21:49:57 +0530

    Enable Motion in poitnter using gerture. Added mouse package to move the mouse and wxPYTHON package to calculate the screen size

--- main.py
+++ main.py
@@ -1,26 +1,36 @@
 import cv2
+import mouse
+import wx
 # import time
 # import numpy as np
 # import math
 import HandTrackingModule as htm
 
+app = wx.App(False)
+width, height = wx.GetDisplaySize()
+
 cap = cv2.VideoCapture(0)
 
 detector = htm.handDetector(detectionCon=0.7)
-
+i = 0
 while True:
     success, img = cap.read()
+    img = cv2.flip(img, 1)
     img = detector.findHands(img)
     cv2.imshow("Img", img)
-    lm = detector.findPosition(img)
-    r = 0.0
-    try:
-        if ((((lm[4][1]-lm[8][1])**2)+((lm[4][2]-lm[8][2])**2))**0.5)/240 > 1:
-            r = 1
-        else:
-            r = ((((lm[4][1]-lm[8][1])**2)+((lm[4][2]-lm[8][2])**2))**0.5)/240
-        print(r)
-    except:
-        print("NO HAND VISIBLE")
+    i += 1
+    if i == 2:  # Adding stability to cursor position
+        lm = detector.findPosition(img)
+        r = 0.0
+        try:
+            mouse.move(lm[8][1] * 2, lm[8][2] * 2)
+            print(((((lm[4][1] - lm[8][1]) ** 2) + ((lm[4][2] - lm[8][2]) ** 2)) ** 0.5) / 240)
+            if ((((lm[4][1] - lm[8][1]) ** 2) + ((lm[4][2] - lm[8][2]) ** 2)) ** 0.5) / 240 < 0.1:
+                mouse.click()
+        except:
+            print("NO HAND VISIBLE")
+        finally:
+            i = 0
+
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
